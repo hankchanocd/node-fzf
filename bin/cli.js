16e18bbea37bdc81f9a5eac8ef19f5c18eaf4793
Hank Chan <hankchanth@icloud.com>
2018-10-29 02:50:07 -0700

    Refactor code

--- bin/cli.js
+++ bin/cli.js
@@ -1,57 +1,55 @@
 #!/usr/bin/env node
 
-const keypress = require( 'keypress' )
+// Dependencies
+const path = require('path');
+const glob = require('redstar');
+const nfzf = require(path.join(__dirname, '../src/index.js'));
+const argv = require('minimist')(process.argv.slice(2));
 
-const fs = require( 'fs' )
-const path = require( 'path' )
-const glob = require( 'redstar' )
 
-const nfzf = require( path.join( __dirname, '../src/index.js' ) )
+(function run() {
+	if (process.stdin.isTTY && !argv._.length) {
+		return glob('**', function (err, files, dirs) {
+			if (err) throw err;
 
-const argv = require( 'minimist' )( process.argv.slice( 2 ) )
+			nfzf(files, function (val, ind) {
+				console.log(files[ind]);
+				process.exit();
+			});
+		});
+	} else {
+		const api = nfzf([], function (val, ind) {
+			console.log(val);
+			process.exit();
+		});
 
-return run()
+		let buffer = '';
+		process.stdin.setEncoding('utf8');
 
-function run ()
-{
-  if ( process.stdin.isTTY && !argv._.length ) {
-    return glob( '**', function ( err, files, dirs ) {
-      if ( err ) throw err
+		process.stdin.on('data', function (chunk) {
+			buffer += chunk;
 
-      nfzf( files, function ( val, ind ) {
-        console.log( files[ ind ] )
-        process.exit()
-      } )
-    } )
-  } else {
-    const api = nfzf( [], function ( val, ind ) {
-      console.log( val )
-      process.exit()
-    } )
+			const list = (
+				buffer.split('\n')
+				.filter(function (t) {
+					return t.trim().length > 0;
+				})
+			);
 
-    let buffer = ''
-    process.stdin.setEncoding( 'utf8' )
+			api.update(list);
+		});
 
-    process.stdin.on( 'data', function ( chunk ) {
-      buffer += chunk
+		process.stdin.on('end', function () {
+			console.log('end');
 
-      const list = (
-        buffer.split( '\n' )
-        .filter( function ( t ) { return t.trim().length > 0 } )
-      )
+			const list = (
+				buffer.split('\n')
+				.filter(function (t) {
+					return t.trim().length > 0;
+				})
+			);
 
-      api.update( list )
-    } )
-
-    process.stdin.on( 'end', function () {
-      console.log( 'end' )
-
-      const list = (
-        buffer.split( '\n' )
-        .filter( function ( t ) { return t.trim().length > 0 } )
-      )
-
-      api.update( list )
-    } )
-  }
-}
+			api.update(list);
+		});
+	}
+})();
